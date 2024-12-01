6405958f19cce1eae76357281b3469b134d821c8
Barun2100 <barunmaji899410@gmail.com>
2024-12-01 15:55:31 +0530

    first commit

---
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.ecom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
