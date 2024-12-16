7ba6a97ab8a006df3a57cc3894b36ca2364b843c
Barun2100 <barunmaji899410@gmail.com>
2024-12-16 11:18:26 +0530

    Add category ui page with backend code

--- src/main/java/com/ecom/controller/AdminController.java
+++ src/main/java/com/ecom/controller/AdminController.java
@@ -1,12 +1,34 @@
 package com.ecom.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.ecom.model.Category;
+import com.ecom.service.CategoryService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping("/")
 	public String index() {
@@ -21,6 +43,41 @@ public class AdminController {
 	@GetMapping("/addcategory")
 	public String addCategory() {
 		return "admin/add-category";
+	}
+
+	@PostMapping("/savecategory")
+	public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, @RequestParam("isActive") boolean isActive,
+			HttpSession session) throws IOException {
+		
+		
+		String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
+		category.setImageName(imageName);
+		boolean existCategory = categoryService.existCategory(category.getName());
+
+		if (existCategory) {
+			session.setAttribute("errorMsg", "Category name already exist");
+		} else {
+			
+			//active status was automatically showing false everytime. even when it was rebdering as true also.
+			category.setActive(isActive);
+
+			Category saveCategory = categoryService.saveCategory(category);
+
+			if (ObjectUtils.isEmpty(saveCategory)) {
+				session.setAttribute("errorMsg", "Not saved ! , internal server error.");
+			} else {
+
+				File savefile = new ClassPathResource("static/images").getFile();
+				Path path = Paths.get(savefile.getAbsolutePath()+File.separator+"category"+File.separator+file.getOriginalFilename());
+				
+				System.out.println("The path     :    "+ path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+				session.setAttribute("succMsg", "saved successfully!");
+			}
+		}
+
+		return "redirect:/admin/addcategory";
 	}
 
 }
